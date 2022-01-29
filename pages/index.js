58885e9f6c491d855ffa19b2acb3c2a266563f26
Volkan Şendağ <vsendag@gmail.com>
2022-01-29 17:40:42 +0000

    test

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,6 @@ export default function Home() {
         <p>Software Architech</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-        <p>
-          daha ne yapılabilir ki ?
-        </p>
-        <p>
-          daha ne yapılabilir ki?
         </p>
       </section>
     </Layout>
